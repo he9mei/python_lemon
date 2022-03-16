9ab38ecb43265f362e446cb7a980deed0ff87991
he9mei <396167189@qq.com>
2022-03-17 00:31:55 +0800

    git秘钥

--- git.py
+++ git.py
@@ -2,6 +2,39 @@
 # -*- coding: utf-8 -*-
 
 '''
+步骤1：Generating a new SSH key
+-->ssh-keygen -t ed25519 -C "396167189@qq.com"
+然后都默认，点击enter即可
+执行结果：
+Your identification has been saved in C:\Users\lipan/.ssh/id_ed25519.
+Your public key has been saved in C:\Users\lipan/.ssh/id_ed25519.pub.
+The key fingerprint is:
+SHA256:idOL5deUd5zFXTjqATYFStA6nvqZJ0AF1cjLIRtVUhw 396167189@qq.com
+
+步骤2：Adding your SSH key to the ssh-agent
+---命令1---
+-->ssh-agent -s
+遇到问题
+Windows 10 启动自带 ssh-agent
+报错 ：unable to start ssh-agent service, error :1058
+解决办法
+用管理员权限打开Power Shell，执行
+Set-Service -Name ssh-agent -StartupType automatic
+---命令2---
+$ ssh-add ~/.ssh/id_ed25519 命令2示例
+-->ssh-add C:\Users\lipan\.ssh\id_ed25519
+执行结果：Identity added: C:\Users\lipan\.ssh\id_ed25519 (396167189@qq.com)
+
+步骤3：Add the SSH key to your account on GitHub
+到Git网页，gitHub点击用户头像，选择setting，
+新建一个SSH Key,取个名字id_ed25519，把id_ed25519.pub拷贝的秘钥复制进去，添加就好啦。
+------------------------------
+以前项目的地址：C:\Users\lipan\PycharmProjects\python_lemon
+使用Git desktop之后，
+克隆url: git@github.com:he9mei/python_lemon.git
+克隆到地址： E:\GitHub\python_lemon
+
+---------------------
 问题：
 push报错：
 Push failed Unable to access 'https://github.com/he9mei/python_lemon.git/':
