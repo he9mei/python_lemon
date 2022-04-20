faeca5b19e6cb7d0a2d4da5e896572a8881428e0
he9mei <396167189@qq.com>
2022-04-20 10:05:49 +0800

    end

--- web/web_po/web_po_v5/Common/dir_config.py
+++ web/web_po/web_po_v5/Common/dir_config.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 import os
 
-base_dir = "/web/web_po/web_po_v4/"  # 不一定对？
 
+# print(os.path.abspath(__file__))  C:\Users\lipan\PycharmProjects\python_lemon\web\web_po\web_po_v5\Common\dir_config.py
+# print(os.getcwd())  C:\Users\lipan\PycharmProjects\python_lemon\web\web_po\web_po_v5\Common
+
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 screenshot_dir = os.path.join(base_dir, "Outputs/screenshots")
-print(screenshot_dir)
+print(screenshot_dir)  # C:\Users\lipan\PycharmProjects\python_lemon\web\web_po\web_po_v5\Outputs/screenshots
